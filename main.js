42c1c56b37194d3015bf5f8b154bf68a58b255e1
Bruno M Belo <bmbelo87@gmail.com>
2021-10-22 12:19:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const linksSocialMedia = {
+  github: 'bmbelo87',
+  youtube: 'brunosilver',
+  facebook: 'brunosilver',
+  instagram: 'brunosilver',
+  twitter: '_brunosilver'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    //Lê os filhos (li) dentro da ID socialLinks.
+    console.log(li.children[0].href) //Verificação das redes sociais.
+    const social = li.getAttribute('class') //Variável social recebe os nomes das classes das li.
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}` //Renomeia os links a partir do primeiro filho.
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  console.log(url)
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLink.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
